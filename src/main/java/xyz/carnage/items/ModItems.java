f8cc72d388acb636bf496c0531fcf09f9f674f7f
dia <diaduckk@outlook.com>
2025-01-02 18:55:35 +0000

    rearranged some curly brackets

--- src/main/java/xyz/carnage/items/ModItems.java
+++ src/main/java/xyz/carnage/items/ModItems.java
@@ -15,14 +15,16 @@ public final class ModItems {
 
     // Declare the item but don't register it yet
     public static final Item BLOOD_ESSENCE;
-
-    static {BLOOD_ESSENCE = Registry.register(Registries.ITEM, Identifier.of(Carnage.MOD_ID, "blood_essence"), new Item(new Item.Settings()));}
+    static {
+        BLOOD_ESSENCE = Registry.register(Registries.ITEM, Identifier.of(Carnage.MOD_ID, "blood_essence"), new Item(new Item.Settings()));
+    }
 
     public static final Item PHANTOMS_KISS;
-
-    static {PHANTOMS_KISS = Registry.register(Registries.ITEM, Identifier.of(Carnage.MOD_ID, "phantoms_kiss"),
+    static {
+        PHANTOMS_KISS = Registry.register(Registries.ITEM, Identifier.of(Carnage.MOD_ID, "phantoms_kiss"),
              new SwordItem(ModToolMaterials.PHANTOMS_KISS, new Item.Settings()
-                     .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.PHANTOMS_KISS, 5, -3.1f))));}
+                     .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.PHANTOMS_KISS, 5, -3.1f))));
+    }
 
     public static void initialize() {
         Carnage.LOGGER.info("Registering items for " + Carnage.MOD_ID);
