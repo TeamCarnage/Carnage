f25fc2b294e196be61e2733b8b6e797c46bf0c0b
Deamozg <deamozmusic@gmail.com>
2025-01-02 18:54:23 +0100

    setting up for new tool system

--- src/main/java/xyz/carnage/items/ModItems.java
+++ src/main/java/xyz/carnage/items/ModItems.java
@@ -14,37 +14,15 @@ public final class ModItems {
     }
 
     // Declare the item but don't register it yet
-    public static final Item PHANTOMS_KISS;
+    public static final Item BLOOD_ESSENCE;
 
     static {
         // Register items in static block
-        PHANTOMS_KISS = Registry.register(
+        BLOOD_ESSENCE = Registry.register(
                 Registries.ITEM,
-                Identifier.of(Carnage.MOD_ID, "phantoms_kiss"),
+                Identifier.of(Carnage.MOD_ID, "blood_essence"),
                 new Item(new Item.Settings())
         );
-    }
-
-    // Are you sure you want to put every item into one file?
-    // Why not make multiple class files in the xyz.carnage.items package?
-    // -DiaDuck
-
-    public class PhantomToolMaterial implements ToolMaterial {
-
-        public int getDurability() { return 4096; }
-        //            Durability = 4096
-        public int getEnchantability() { return 15; }
-        //            Enchantability = 15
-
-        public float getMiningSpeedMultiplier() { return 0.0f; }
-        //              Mining Speed Multiplier = 0.0
-        public float getAttackDamage() { return 10.0f; }
-        //              Attack Damage = 10.0
-
-        // nulls:
-        public TagKey<Block> getInverseTag() { return null; }
-        public Ingredient getRepairIngredient() { return null; }
-
     }
 
     public static void initialize() {
