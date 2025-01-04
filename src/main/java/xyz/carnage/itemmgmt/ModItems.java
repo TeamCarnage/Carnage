3bbc9e7b4d4c1b8e69c612cbf7dca4efd9858ece
DiaDuck <diaduckk@outlook.com>
2025-01-04 21:33:00 +0000

    fixed brinebreaker issues! ready for development :3

--- src/main/java/xyz/carnage/itemmgmt/ModItems.java
+++ src/main/java/xyz/carnage/itemmgmt/ModItems.java
@@ -21,6 +21,7 @@ public final class ModItems {
     public static final Item BLOOD_ESSENCE;
     public static final Item PHANTOMS_KISS;
     public static final Item BLAZERENDER;
+    public static final Item BRINEBREAKER;
 
     static {
         BLOOD_ESSENCE = Registry.register(Registries.ITEM,
@@ -42,6 +43,13 @@ public final class ModItems {
                 new BlazerenderItem(ModToolMaterials.BLAZERENDER,
                         new Item.Settings().attributeModifiers(
                                 SwordItem.createAttributeModifiers(ModToolMaterials.BLAZERENDER,3,-2.3f))));
+
+        BRINEBREAKER = Registry.register(Registries.ITEM,
+                Identifier.of(Carnage.MOD_ID, "brinebreaker"),
+                new BrinebreakerItem(ModToolMaterials.BRINEBREAKER,
+                        new Item.Settings().attributeModifiers(
+                                TridentItem.createAttributeModifiers())));
+
 
     }
 
--- src/main/java/xyz/carnage/itemmgmt/items/BrinebreakerItem.java
+++ src/main/java/xyz/carnage/itemmgmt/items/BrinebreakerItem.java
@@ -1,4 +1,16 @@
 package xyz.carnage.itemmgmt.items;
 
-public class BrinebreakerItem {
+import net.minecraft.item.TridentItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
+
+public class BrinebreakerItem extends TridentItem {
+    public BrinebreakerItem(ToolMaterial material, Item.Settings settings) {
+        super(settings);
+
+
+        // code the item lol
+
+
+    }
 }
