19ee8f22cb769a9d032c7eda5b8d56a83a85aa44
DiaDuck <diaduckk@outlook.com>
2025-01-04 21:44:16 +0000

    commented out line 20 on ModToolMaterials.java

--- src/main/java/xyz/carnage/itemmgmt/ModToolMaterials.java
+++ src/main/java/xyz/carnage/itemmgmt/ModToolMaterials.java
@@ -17,7 +17,7 @@ public enum ModToolMaterials implements ToolMaterial {
             () -> Ingredient.ofItems(Items.BLAZE_ROD)),
     BRINEBREAKER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 2031, 10.0F, 4.0F, 20,
             () -> Ingredient.ofItems(Items.NETHERITE_INGOT));
-            () -> Ingredient.ofItems(Items.BLAZE_ROD));
+            //() -> Ingredient.ofItems(Items.BLAZE_ROD));
 
 
     private final TagKey<Block> inverseTag;
