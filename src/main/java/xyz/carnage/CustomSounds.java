73e11bb1c276fbea04f50c3bcfbb1e4bc2f3ba74
ColonelNeon <94647617+ColonelNeon@users.noreply.github.com>
2025-01-24 22:20:21 +0000

    Added playSound function

--- src/main/java/xyz/carnage/CustomSounds.java
+++ src/main/java/xyz/carnage/CustomSounds.java
@@ -1,5 +1,6 @@
 package xyz.carnage;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundCategory;
@@ -9,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,6 +35,7 @@ public class CustomSounds {
     public static void playItemSound(String eventType, ItemStack stack, World world, PlayerEntity player) {
         String itemId = Registries.ITEM.getId(stack.getItem()).toString();
         Identifier soundId = ITEM_SOUNDS.getOrDefault(itemId, new HashMap<>()).get(eventType);
+
 
         if (soundId != null && SOUND_EVENTS.containsKey(soundId)) {
             SoundEvent soundEvent = SOUND_EVENTS.get(soundId);
@@ -59,12 +62,42 @@ public class CustomSounds {
         }
     }
 
+    public static void playSound(Entity source, @NotNull PlayerEntity player, String soundName, SoundCategory soundCategory, Float volume, Float pitch) {
+
+        /*
+        *   USAGE:
+        *   source - is usually null.
+        *   player - sets the location that the sound comes from, use the player entity that you want to hear the sound.
+        *   soundName - is the file name of the sound. Use the name of the sound file e.g "phantoms_kiss_hit".
+        *   soundCategory - is the sound category you want the sound to play, adjustable by the players in the settings menu. e.g SoundCategory.PLAYER
+        *   volume - is how loud the sound is, usually set to 1.
+        *   pitch - is the pitch of the sound (duh) usually set to 1.
+        *
+        *   Example: playSound(null, player, "soundName", SoundCategory.PLAYER, 1.0F, 1.0F);
+        *
+        * */
+
+        SoundEvent soundEvent = SOUND_EVENTS.get(Identifier.of(MOD_ID, soundName));
+        player.getWorld().playSound(
+                source,
+                player.getBlockPos(),
+                soundEvent,
+                soundCategory,
+                volume,
+                pitch
+        );
+    }
+
     public static void registerSounds(String modId) {
         // Register Sounds (not for the events).
+
+        // Usage: registerSound(modId, "item ID - same as in ModItems")
         registerSound(modId, "phantoms_kiss_hit");
         registerSound(modId, "surge_discharge");
 
         // Register sound events (theres two types of event - hit and break - they're pretty self-explanitory) - dont use "break" eventType, it does nothing atm.
+
+        // USAGE: registerItemSound("mod_id:item_name", "eventType", "sound file name", modId);
         registerItemSound("carnage:phantoms_kiss", "hit", "phantoms_kiss_hit", modId);
         registerItemSound("carnage:surge", "hit", "surge_discharge", modId);
     }
