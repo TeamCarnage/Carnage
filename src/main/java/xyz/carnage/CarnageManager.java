3d5cfd0cde6d861efc995032a14f8704bc9c525f
Deamozg <deamozmusic@gmail.com>
2025-02-08 19:55:24 +0100

    detection logic for brineshields work

--- src/main/java/xyz/carnage/CarnageManager.java
+++ src/main/java/xyz/carnage/CarnageManager.java
@@ -1,6 +1,9 @@
 package xyz.carnage;
 
+import net.minecraft.entity.EntityType;
 import xyz.carnage.manager.combo.ComboEventHandler;
+import xyz.carnage.manager.entity.brinebreaker.BrinebreakerEntity;
+import xyz.carnage.manager.entity.entityCombos.BrinebreakerEntityCombos;
 import xyz.carnage.manager.item.CarnageItemGroups;
 import xyz.carnage.manager.item.CarnageItems;
 import xyz.carnage.manager.particle.CustomParticles;
@@ -13,5 +16,6 @@ public class CarnageManager {
         CarnageItemGroups.initialize();
         SoundManager.initialize();
         ComboEventHandler.initialize();
+        BrinebreakerEntityCombos.initialise();
     }
 }
